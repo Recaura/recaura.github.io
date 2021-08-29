86feb3d9a0f69b8e9c53bb02a330725d1cd355cc
Jeff H <78248407+jeffrey-huang-recaura@users.noreply.github.com>
2021-08-29 21:42:36 +1000

    fix privacy policy app name

--- src/components/legal/PrivacyPolicyPage.jsx
+++ src/components/legal/PrivacyPolicyPage.jsx
@@ -4,7 +4,7 @@ function PrivacyPolicyPage() {
   return (
     <>
       <h1>Privacy Policy</h1>
-      <p>Last updated: March 29, 2021</p>
+      <p>Last updated: August 29, 2021</p>
       <p>
         This Privacy Policy describes Our policies and procedures on the collection, use and disclosure of Your
         information when You use the Service and tells You about Your privacy rights and how the law protects You.
@@ -38,7 +38,7 @@ function PrivacyPolicyPage() {
         <li>
           <p>
             <strong>Application</strong> means the software program provided by the Company downloaded by You on any
-            electronic device, named Don't Think Just Drink
+            electronic device, named Who Drinks
           </p>
         </li>
         <li>
